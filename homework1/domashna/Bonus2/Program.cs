437fab740cafde0fd6e63fec196f33c2630812ab
ceca22 <78369030+ceca22@users.noreply.github.com>
2021-03-26 11:29:52 +0100

    hw

---
+++ homework1/domashna/Bonus2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bonus2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
