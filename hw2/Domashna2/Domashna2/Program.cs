6993664d2567d35954a3967d2cdfbdafffc5216c
ceca22 <78369030+ceca22@users.noreply.github.com>
2021-03-30 20:33:00 +0200

    class03

---
+++ hw2/Domashna2/Domashna2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Class03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Domashna od class03-exercise6");
+
+            Console.WriteLine("Enter a name: ");
+            string nameOne = Console.ReadLine();
+            int counter = 1;
+
+
+            //DALI ima nekoj nacin na deklariranje niza bez da odredime kolku elementi se ocekuvaat?
+            //jas probav kako broj da ja stavam varijablata counter, no mi najavi greshka deka mora da vnesam fiksna brojka
+            //a vo ovoj slucaj ne mozam da znam kolku iminja ke vnesuva user-ot
+            string[] nameArray = new string[30];
+
+            nameArray[0] = nameOne;
+
+            for (; ; )
+            {
+                Console.WriteLine("Would you like to enter another name? Answer with Y or N");
+                string answer = Console.ReadLine();
+
+
+                if (answer == "Y")
+                {
+                    Console.WriteLine("Enter " + counter + " name: ");
+                    nameArray[counter] = Console.ReadLine();
+                    counter++;
+
+                }
+
+
+                if (answer == "N")
+                {
+                    Console.WriteLine("You entered N and I'm writing down all the names");
+                    for (int i = 0; i < nameArray.Length; i++)
+                    {
+                        Console.WriteLine(nameArray[i]);
+                    }
+                    break;
+                };
+
+
+
+            };
+        }
+    }
+}
---
+++ hw2/Domashna2/SumOfEven/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] arrayOne = new int[6];
+            int sumOfEven = 0;
+
+            for(int i = 0; i < arrayOne.Length; i++)
+            {
+                Console.WriteLine("Enter a number:");
+                bool input = int.TryParse(Console.ReadLine(), out arrayOne[i]);
+                if(arrayOne[i] % 2 == 0)
+                {
+                    sumOfEven += arrayOne[i];
+                }
+            }
+
+            Console.WriteLine("First input is: " + arrayOne[0]);
+            Console.WriteLine("Second input is: " + arrayOne[1]);
+            Console.WriteLine("Third input is: " + arrayOne[2]);          
+            Console.WriteLine("Forth input is: " + arrayOne[3]);            
+            Console.WriteLine("Fifth input is: " + arrayOne[4]);            
+            Console.WriteLine("Six input is: " + arrayOne[5]);           
+            
+
+            Console.WriteLine("The sum of even numbers is: " + sumOfEven);
+
+        }
+    }
+}
