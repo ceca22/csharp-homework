faa7718a470004a869341e349f1976570fe730bd
ceca22 <78369030+ceca22@users.noreply.github.com>
2021-04-04 23:03:54 +0200

    hw class4

--- class04/vezba2/Program.cs
+++ class04/vezba2/Program.cs
@@ -5,33 +5,22 @@ namespace vezba2
 {
     class Program
     {
-        public void replace(string str1)                         //create function
-
-        {
-
-            string pattern = "\\s+";
-
-            string replacement = " ";                       // replacement pattern
-
-            Regex rx = new Regex(pattern);
-
-            string result = rx.Replace(str1, replacement);  // call to replace space
-
-            Console.WriteLine("Your string String          : {0}", str1);
-
-            Console.WriteLine("Remove extra space String   : {0}", result);
-
-
-
-        }
+        
 
         
         static void Main(string[] args)
         {
             string myText = " The    best  Lorem  Ipsum        Generator in all the  sea!   Heave this   scurvy copyfiller fer yar         next   adventure  and cajol yar clients   into walking the plank with ev'ry layout!    Configure       above, then get yer pirate ipsum...own the high seas,    argh!";
-            
 
 
+            string[] text = myText.Split();
+            Console.WriteLine(text);
+
+            foreach(string item in text)
+            {
+                string trimmed = item.Trim();
+                Console.Write(trimmed);
+            }
         }
     }
 }
