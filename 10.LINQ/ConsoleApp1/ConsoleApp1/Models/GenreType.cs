dd4ab70cc8bb0f4edd29fbe7954dba3979c09809
ceca22 <78369030+ceca22@users.noreply.github.com>
2021-08-16 13:37:53 +0200

    update hw c#

---
+++ 10.LINQ/ConsoleApp1/ConsoleApp1/Models/GenreType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public enum Genre
+    {
+        PopRock,
+        Pop,
+        Rock,
+        RnB
+    }
+}
---
+++ 10.LINQ/ConsoleApp1/ConsoleApp1/Models/Song.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public int AlbumId { get; set; }
+        public string Name { get; set; }
+        /// <summary>
+        /// duration in seconds
+        /// </summary>
+        public int Duration { get; set; }
+
+        public Song() { }
+
+        public Song(int id, int albumId, string name, int duration)
+        {
+            Id = id;
+            AlbumId = albumId;
+            Name = name;
+            Duration = duration;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
